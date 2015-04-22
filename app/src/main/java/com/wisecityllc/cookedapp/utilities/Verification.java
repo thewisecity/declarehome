ba29ce078b3f1451ac853677aed15fde55aee49a
dexterlohnes <dexterlohnes@gmail.com>
2015-04-21 22:38:13 -0700

    +Added Verification static library

---
+++ app/src/main/java/com/wisecityllc/cookedapp/utilities/Verification.java
@@ -0,0 +1,21 @@
+package com.wisecityllc.cookedapp.utilities;
+
+/**
+ * Created by dexterlohnes on 4/21/15.
+ */
+public class Verification {
+    public static boolean verifyEmail(String email){
+        //TODO: implement
+        return true;
+    }
+
+    public static boolean verifyPassword(String password){
+        //TODO: implement
+        return true;
+    }
+
+    public static boolean verifyUsername(String username){
+        //TODO: implement
+        return true;
+    }
+}
