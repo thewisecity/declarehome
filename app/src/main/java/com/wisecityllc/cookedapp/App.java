bf846dbe1359023abb4fc5fbc20e96f15b93cb97
dexterlohnes <dexterlohnes@gmail.com>
2015-07-06 12:24:58 -0700

    +Added ability to get app context from static master instance of App object

--- app/src/main/java/com/wisecityllc/cookedapp/App.java
+++ app/src/main/java/com/wisecityllc/cookedapp/App.java
@@ -1,6 +1,7 @@
 package com.wisecityllc.cookedapp;
 
 import android.app.Application;
+import android.content.Context;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
@@ -11,11 +12,18 @@ import com.wisecityllc.cookedapp.parseClasses.Group;
  */
 public class App extends Application {
 
+    private static App instance;
+
     @Override
     public void onCreate() {
         super.onCreate();
+        instance = this;
         Parse.enableLocalDatastore(this);
         ParseObject.registerSubclass(Group.class);
         Parse.initialize(this, "BrndBVrRczElKefgG3TvjCk3JYxtd5GB2GMzKoEP", "Xb7Pcc0lT2I3uJYNNoT6buaCuZ9dcvBMtCx9U5gw");
+    }
+
+    public static Context getContext() {
+        return instance;
     }
 }
