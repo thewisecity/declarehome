5afde035c0ca0fc459b6d0b165e7e5914e893aad
dexterlohnes <dexterlohnes@gmail.com>
2015-08-07 15:42:23 -0700

    +Updated Parse library to latest (1.9.4) from previous (1.9.1) +Added Fabric/Crashlytics to application

--- app/src/main/java/com/wisecityllc/cookedapp/App.java
+++ app/src/main/java/com/wisecityllc/cookedapp/App.java
@@ -3,9 +3,12 @@ package com.wisecityllc.cookedapp;
 import android.app.Application;
 import android.content.Context;
 
+import com.crashlytics.android.Crashlytics;
 import com.parse.Parse;
+import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.wisecityllc.cookedapp.parseClasses.Group;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by dexterlohnes on 4/25/15.
@@ -17,10 +20,12 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         instance = this;
         Parse.enableLocalDatastore(this);
         ParseObject.registerSubclass(Group.class);
         Parse.initialize(this, "BrndBVrRczElKefgG3TvjCk3JYxtd5GB2GMzKoEP", "Xb7Pcc0lT2I3uJYNNoT6buaCuZ9dcvBMtCx9U5gw");
+        ParseInstallation.getCurrentInstallation().saveInBackground();
     }
 
     public static Context getContext() {
