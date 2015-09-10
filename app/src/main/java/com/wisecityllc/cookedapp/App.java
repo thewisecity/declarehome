7de84f961f337fadbe0779e607550e4766859ad1
dexterlohnes <dexterlohnes@gmail.com>
2015-09-09 21:06:08 -0700

    +Added public static universally accessible function to close keyboard

--- app/src/main/java/com/wisecityllc/cookedapp/App.java
+++ app/src/main/java/com/wisecityllc/cookedapp/App.java
@@ -1,7 +1,10 @@
 package com.wisecityllc.cookedapp;
 
+import android.app.Activity;
 import android.app.Application;
 import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import com.crashlytics.android.Crashlytics;
 import com.parse.Parse;
@@ -9,6 +12,7 @@ import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.wisecityllc.cookedapp.parseClasses.Group;
 import com.wisecityllc.cookedapp.parseClasses.Message;
+
 import io.fabric.sdk.android.Fabric;
 
 /**
@@ -28,6 +32,17 @@ public class App extends Application {
         ParseObject.registerSubclass(Message.class);
         Parse.initialize(this, "BrndBVrRczElKefgG3TvjCk3JYxtd5GB2GMzKoEP", "Xb7Pcc0lT2I3uJYNNoT6buaCuZ9dcvBMtCx9U5gw");
         ParseInstallation.getCurrentInstallation().saveInBackground();
+    }
+
+    public static void hideKeyboard(Activity act) {
+        // Check if no view has focus:
+        if(act != null) {
+            View view = act.getCurrentFocus();
+            if (view != null && instance != null) {
+                InputMethodManager imm = (InputMethodManager) instance.getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        }
     }
 
     public static Context getContext() {
