a9d4282a922e7ef7cd2ea9bed31ceca74a7b83fc
dexterlohnes <dexterlohnes@gmail.com>
2015-09-21 10:52:57 -0700

    +Trying to make Group use local cache effectively

--- app/src/main/java/com/wisecityllc/cookedapp/parseClasses/Group.java
+++ app/src/main/java/com/wisecityllc/cookedapp/parseClasses/Group.java
@@ -12,11 +12,13 @@ import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
+import com.parse.ParseRole;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
 import com.wisecityllc.cookedapp.App;
 import com.wisecityllc.cookedapp.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -34,6 +36,9 @@ public class Group extends ParseObject {
     final static String _WEBSITE = "website";
     final static String _FACEBOOK = "facebook";
     final static String _TWITTER = "twitter";
+
+    private boolean mUseLocalStorageForMembers = false;
+    private boolean mUseLocalStorageForAdmins = false;
 
     public Group() {
         // A default constructor is required.
@@ -182,6 +187,10 @@ public class Group extends ParseObject {
         put("twitter", twitter);
     }
 
+    public ParseRole getMembersRole() { return (ParseRole)getParseObject("membersRole"); }
+
+    public ParseRole getAdminsRole() { return (ParseRole)getParseObject("adminsRole"); }
+
     public static void refreshLocalGroupPermissions() {
         ParseUser currentUser = ParseUser.getCurrentUser();
         currentUser.fetchInBackground(new GetCallback<ParseUser>() {
@@ -221,66 +230,257 @@ public class Group extends ParseObject {
         });
     }
 
+//    public boolean isCurrentUserMember() {
+//        ParseUser currentUser;
+//        try {
+//            currentUser = ParseUser.getCurrentUser();
+//            currentUser.fetchFromLocalDatastore();
+//
+//        } catch (ParseException e) {
+//            Toast.makeText(App.getContext(), "Couldn't find local version of user", Toast.LENGTH_SHORT).show();
+//            e.printStackTrace();
+//            return false;
+//        }
+//        try {
+//
+//            ParseRelation<Group> memberOf = currentUser.getRelation("memberOf");
+//
+//            ParseQuery memberQuery = memberOf.getQuery().
+//                    fromLocalDatastore().
+//                    whereEqualTo("objectId", this.getObjectId());
+//
+//            Group memberGroup =  (Group) memberQuery.getFirst();
+//            if(memberGroup != null)
+//                return true;
+//            else
+//                return false;
+//
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+//
+//    public boolean isCurrentUserAdmin() {
+//        ParseUser currentUser;
+//        try {
+//            currentUser = ParseUser.getCurrentUser();
+//            currentUser.fetchFromLocalDatastore();
+//
+//        } catch (ParseException e) {
+//            Toast.makeText(App.getContext(), "Couldn't find local version of user", Toast.LENGTH_SHORT).show();
+//            e.printStackTrace();
+//            return false;
+//        }
+//
+//        try{
+//
+//            ParseRelation<Group> adminOf = currentUser.getRelation("adminOf");
+//
+//            ParseQuery adminQuery = adminOf.getQuery().
+//                    fromLocalDatastore().
+//                    whereEqualTo("objectId", this.getObjectId());
+//
+//            Group adminGroup =  (Group) adminQuery.getFirst();
+//            if(adminGroup != null)
+//                return true;
+//            else
+//                return false;
+//
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//    }
+
     public boolean isCurrentUserMember() {
-        ParseUser currentUser;
-        try {
-            currentUser = ParseUser.getCurrentUser();
-            currentUser.fetchFromLocalDatastore();
-
-        } catch (ParseException e) {
-            Toast.makeText(App.getContext(), "Couldn't find local version of user", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-            return false;
-        }
-        try {
-
-            ParseRelation<Group> memberOf = currentUser.getRelation("memberOf");
-
-            ParseQuery memberQuery = memberOf.getQuery().
-                    fromLocalDatastore().
-                    whereEqualTo("objectId", this.getObjectId());
-
-            Group memberGroup =  (Group) memberQuery.getFirst();
-            if(memberGroup != null)
-                return true;
-            else
-                return false;
-
-        } catch (ParseException e) {
-            e.printStackTrace();
-            return false;
-        }
+        boolean isMember = isUserMember(ParseUser.getCurrentUser(), false);
+        return isMember;
     }
 
     public boolean isCurrentUserAdmin() {
-        ParseUser currentUser;
-        try {
-            currentUser = ParseUser.getCurrentUser();
-            currentUser.fetchFromLocalDatastore();
+        boolean isAdmin = isUserAdmin(ParseUser.getCurrentUser(), false);
+        return isAdmin;
+    }
 
-        } catch (ParseException e) {
-            Toast.makeText(App.getContext(), "Couldn't find local version of user", Toast.LENGTH_SHORT).show();
-            e.printStackTrace();
-            return false;
-        }
+    public boolean isUserAdmin(ParseUser user, boolean forceServerContact) {
+
+        boolean userIsAdmin = false;
 
         try{
 
-            ParseRelation<Group> adminOf = currentUser.getRelation("adminOf");
+            if(mUseLocalStorageForAdmins == true && forceServerContact == false){
+                ParseRole adminsRole = this.getAdminsRole();
+                adminsRole.fetchFromLocalDatastore();
+                ParseRelation<ParseUser> admins = adminsRole.getUsers();
+//                ParseQuery adminQuery = adminOf.getQuery()
+                ParseQuery<ParseUser> adminQuery = admins.getQuery()
+                        .whereEqualTo("objectId", user.getObjectId())
+                        .fromLocalDatastore();
 
-            ParseQuery adminQuery = adminOf.getQuery().
-                    fromLocalDatastore().
-                    whereEqualTo("objectId", this.getObjectId());
-
-            Group adminGroup =  (Group) adminQuery.getFirst();
-            if(adminGroup != null)
-                return true;    
-            else
-                return false;
+                ParseUser userAsAdmin =  (ParseUser) adminQuery.getFirst();
+                if(userAsAdmin != null)
+                    userIsAdmin = true;
+                else
+                    userIsAdmin = false;
+            } else {
+                ParseRole adminsRole = this.getAdminsRole();
+                adminsRole.fetch();
+                adminsRole.pin();
+                ParseRelation<ParseUser> admins = adminsRole.getUsers();
+                // We haven't retrieved or we're being forced to contact server
+                // Get all members
+                ParseQuery<ParseUser> adminQuery = admins.getQuery();
+                ArrayList<ParseUser> adminList = new ArrayList<ParseUser>(adminQuery.find());
+                // Loop through all of our members, pinning all of them for future use
+                for(ParseUser admin : adminList) {
+                    admin.pin();
+                    // If we come across the user we are searching for, we want to return that they are a member
+                    // but not before we finishing pinning all of the rest of the members
+                    if(admin.getObjectId().equalsIgnoreCase(user.getObjectId()))
+                        userIsAdmin = true;
+                }
+                mUseLocalStorageForAdmins = true;
+            }
 
         } catch (ParseException e) {
             e.printStackTrace();
             return false;
         }
+
+        return userIsAdmin;
     }
+
+    public boolean isUserMember(ParseUser user, boolean forceServerContact) {
+
+        boolean userIsMember = false;
+
+        try{
+
+            if(mUseLocalStorageForMembers == true && forceServerContact == false){
+                ParseRole membersRole = this.getMembersRole();
+                membersRole.fetchFromLocalDatastore();
+                ParseRelation<ParseUser> members = membersRole.getUsers();
+//                ParseQuery adminQuery = adminOf.getQuery()
+                ParseQuery<ParseUser> memberQuery = members.getQuery()
+                        .whereEqualTo("objectId", user.getObjectId())
+                        .fromLocalDatastore();
+
+                ParseUser userAsMember =  (ParseUser) memberQuery.getFirst();
+                if(userAsMember != null)
+                    userIsMember = true;
+                else
+                    userIsMember = false;
+            } else {
+                ParseRole membersRole = this.getMembersRole();
+                membersRole.fetch();
+                membersRole.pin();
+                ParseRelation<ParseUser> members = membersRole.getUsers();
+                // We haven't retrieved or we're being forced to contact server
+                // Get all members
+                ParseQuery<ParseUser> memberQuery = members.getQuery();
+                ArrayList<ParseUser> memberList = new ArrayList<ParseUser>(memberQuery.find());
+                // Loop through all of our members, pinning all of them for future use
+                for(ParseUser member : memberList) {
+                    member.pin();
+                    // If we come across the user we are searching for, we want to return that they are a member
+                    // but not before we finishing pinning all of the rest of the members
+                    if(member.getObjectId().equalsIgnoreCase(user.getObjectId()))
+                        userIsMember = true;
+                }
+                mUseLocalStorageForMembers = true;
+            }
+
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return userIsMember;
+    }
+
+//    public boolean isUserAdmin(ParseUser user, boolean forceServerContact) {
+//
+//        boolean userIsAdmin = false;
+//
+//        try{
+//
+//            ParseRelation<Group> adminOf = user.getRelation("adminOf");
+//
+//            if(mUseLocalStorageForAdmins == true && forceServerContact == false){
+//                ParseQuery adminQuery = adminOf.getQuery()
+//                        .whereEqualTo("objectId", this.getObjectId())
+//                        .fromLocalDatastore();
+//
+//                Group adminGroup =  (Group) adminQuery.getFirst();
+//                if(adminGroup != null)
+//                    userIsAdmin = true;
+//                else
+//                    userIsAdmin = false;
+//            } else {
+//                // We haven't retrieved or we're being forced to contact server
+//                // Get all members
+//                ParseQuery adminQuery = adminOf.getQuery();
+//                ArrayList<ParseUser> adminList = new ArrayList<ParseUser>(adminQuery.find());
+//                // Loop through all of our members, pinning all of them for future use
+//                for(ParseUser admin : adminList) {
+//                    admin.pin();
+//                    // If we come across the user we are searching for, we want to return that they are a member
+//                    // but not before we finishing pinning all of the rest of the members
+//                    if(admin.getObjectId().equalsIgnoreCase(user.getObjectId()))
+//                        userIsAdmin = true;
+//                }
+//                mUseLocalStorageForAdmins = true;
+//            }
+//
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//
+//        return userIsAdmin;
+//    }
+//
+//    public boolean isUserMember(ParseUser user, boolean forceServerContact) {
+//
+//        boolean userIsMember = false;
+//
+//        try{
+//
+//            ParseRelation<Group> memberOf = user.getRelation("memberOf");
+//
+//            if(mUseLocalStorageForMembers == true && forceServerContact == false){
+//                ParseQuery memberQuery = memberOf.getQuery()
+//                        .whereEqualTo("objectId", this.getObjectId())
+//                        .fromLocalDatastore();
+//
+//                Group memberGroup =  (Group) memberQuery.getFirst();
+//                if(memberGroup != null)
+//                    userIsMember = true;
+//                else
+//                    userIsMember = false;
+//            } else {
+//                // We haven't retrieved or we're being forced to contact server
+//                // Get all members
+//                ParseQuery memberQuery = memberOf.getQuery();
+//                ArrayList<ParseUser> memberList = new ArrayList<ParseUser>(memberQuery.find());
+//                // Loop through all of our members, pinning all of them for future use
+//                for(ParseUser member : memberList) {
+//                    member.pin();
+//                    // If we come across the user we are searching for, we want to return that they are a member
+//                    // but not before we finishing pinning all of the rest of the members
+//                    if(member.getObjectId().equalsIgnoreCase(user.getObjectId()))
+//                        userIsMember = true;
+//                }
+//
+//                mUseLocalStorageForMembers = true;
+//            }
+//
+//        } catch (ParseException e) {
+//            e.printStackTrace();
+//            return false;
+//        }
+//
+//        return userIsMember;
+//    }
 }
